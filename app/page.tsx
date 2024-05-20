d48bd4161342b8c26c6558d4bc52849c748e1747
Vamsi Krishna Ummadi <vamsiu@italentdigital.com>
2024-05-20 23:29:33 +0530

    Projects Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 import {FaHome} from "react-icons/fa";
 
@@ -8,9 +10,10 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
-        <FloatingNav  navItems={[{name: "Home", icon: <FaHome />, link : "/"}]}/>
+        <FloatingNav  navItems={navItems}/>
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
