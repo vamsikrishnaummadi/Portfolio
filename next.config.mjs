4ef64d926460b265c532ea45ed0667aa240ed1ea
vamsikrishnaummadi <krishnavamsi9432@gmail.com>
2024-06-04 23:37:38 +0530

    next config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript : {
+        output : 'export',
+        ignoreBuildErrors : true
+     }
+};
 
 export default nextConfig;
